d6b2fdb9d5b66023db4753897ae2484025464b82
reyhanmaha <reyhan12maharaj@gmail.com>
2023-11-05 16:48:02 -0400

    added apache dependencies

--- autograding/src/main/java/autograding/App.java
+++ autograding/src/main/java/autograding/App.java
@@ -4,10 +4,21 @@ package autograding;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static SubmissionReader javaReader = new SubmissionReader();
+    public static SpecReader specReader = new SpecReader();
+
+    public static void main(String[] args) {
+        // Specify the path to the uploaded ZIP file.
+        String javaZipFilePath = "path_to_uploaded_zip_file.zip";
+
+        // Specify the directory where you want to extract the submissions.
+        String javaExtractionDirectory = "extraction_directory";
+
+        String specFilePath = "AutoGradingApp\\ProjectTestData.zip";
+        String specExtractionDirectory = "extraction_directory";
+        // Call the method to process the submissions.
+        specReader.readFile(specFilePath, specExtractionDirectory);
+        // javaReader.readFile(javaZipFilePath, javaExtractionDirectory);
     }
 }
