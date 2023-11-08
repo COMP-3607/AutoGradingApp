3f318fdb3492759d9914864a3a514fbaa9999daf
reyhanmaha <reyhan12maharaj@gmail.com>
2023-11-08 15:22:06 -0400

    trying text analysis

--- autograding/src/main/java/autograding/TestSpecCreator.java
+++ autograding/src/main/java/autograding/TestSpecCreator.java
@@ -3,7 +3,11 @@ package autograding;
 public class TestSpecCreator extends TestObjectCreator {
     private Character ch;
     private int count = 0;
+    private int num;
     private int len;
+    private String className;
+    private String temp = "";
+    private char[] x;
 
     public TestSpecCreator() {
     }
@@ -12,9 +16,26 @@ public class TestSpecCreator extends TestObjectCreator {
     public TestObject createTestObject(String specText) {
         // ch=specText.charAt(0);
         len = specText.length();
-        System.out.println("HEREE");
         while (count != len) {
-            System.out.print(specText.charAt(count));
+            // System.out.print(specText.charAt(count));
+            if (specText.charAt(count) == 'C' && specText.charAt(count = count + 1) == 'l') {
+                // if (specText.charAt(count) == 'C') {
+                System.out.println("HEREE");
+                num = count;
+                while (num != 0) {
+                    if (specText.charAt(num) != ' ') {
+                        while (specText.charAt(num) != ' ') {
+                            temp = temp.concat(Character.toString(specText.charAt(num)));
+                            num--;
+                        }
+                        for (int i = 0; i < temp.length(); i++) {
+                            className = temp.charAt(i) + className;
+                        }
+                        System.out.println("Class Name: " + className);
+                    }
+                    num--;
+                }
+            }
             count = count + 1;
         }
 
